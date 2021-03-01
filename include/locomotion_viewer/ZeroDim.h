6a760ee7c8d4ee4ff7d554beb5dc369029c02b2a
Wolfgang Merkt <w.merkt@gmail.com>
2021-03-01 23:18:27 +0000

    Fix ZeroDim

--- include/locomotion_viewer/ZeroDim.h
+++ include/locomotion_viewer/ZeroDim.h
@@ -25,21 +25,22 @@ namespace locomotion_viewer {
         ~ZeroDim();
 
 
-        bool publishEigenSpheres(Eigen::VectorXd & eigen_path_x,
-                                 Eigen::VectorXd & eigen_path_y,
-                                 Eigen::VectorXd & eigen_path_z,
+        bool publishEigenSpheres(const Eigen::VectorXd & eigen_path_x,
+                                 const Eigen::VectorXd & eigen_path_y,
+                                 const Eigen::VectorXd & eigen_path_z,
                                  rviz_visual_tools::colors color,
                                  rviz_visual_tools::scales scale,
                                  const std::string & ns = "Spheres");
 
+        // NB: As soon as I turn this into const-ref for Eigen::Vector3d it has a symbol lookup error. Dont know why
         bool publishEigenSphere(Eigen::Vector3d & point,
                                 rviz_visual_tools::colors color = rviz_visual_tools::GREEN,
                                 rviz_visual_tools::scales scale = rviz_visual_tools::XLARGE,
                                 const std::string & ns = "Sphere");
 
-        double Point2isRightOfLine(const Eigen::Vector3d p0,
-                                          const Eigen::Vector3d p1,
-                                          const Eigen::Vector3d p2)
+        double Point2isRightOfLine(const Eigen::Vector3d & p0,
+                                   const Eigen::Vector3d & p1,
+                                   const Eigen::Vector3d & p2)
         {
           return (p2(1) - p0(1)) * (p1(2) - p0(2))
                  - (p1(1) - p0(1)) * (p2(2) - p0(2));
@@ -54,20 +55,17 @@ namespace locomotion_viewer {
               //the point p2 should always be on the right to be cwise thus if it
               //is on the left <0 i swap
               if (Point2isRightOfLine(p[0], p[i], p[j])  < 0.0) {
-                Eigen::Vector3d tmp = p[i];
-                p[i] = p[j];
-                p[j] = tmp;
+                std::swap(p[i], p[j]);
+                // Eigen::Vector3d tmp = p[i];
+                // p[i] = p[j];
+                // p[j] = tmp;
               }
             }
           }
         }
-
-    private:
-
     };
 
     typedef std::shared_ptr<ZeroDim> ZeroDimPtr;
-
 } // locomotion_viewer
 
 #endif //LOCOMOTION_VIEWER_ZERODIM_H
--- src/ZeroDim.cpp
+++ src/ZeroDim.cpp
@@ -5,7 +5,6 @@
 #include <locomotion_viewer/ZeroDim.h>
 
 namespace locomotion_viewer {
-
     ZeroDim::ZeroDim(std::string base_frame,
                    std::string marker_topic,
                    ros::NodeHandle nh) : RvizVisualTools(base_frame, marker_topic, nh) {}
@@ -13,46 +12,37 @@ namespace locomotion_viewer {
     ZeroDim::~ZeroDim() {}
 
     bool ZeroDim::publishEigenSphere(Eigen::Vector3d & point,
-                                               rviz_visual_tools::colors color,
-                                               rviz_visual_tools::scales scale,
-                                               const std::string & ns)
+                                     rviz_visual_tools::colors color,
+                                     rviz_visual_tools::scales scale,
+                                     const std::string & ns)
     {
         geometry_msgs::Point temp;
-        temp.x = point(0);
-        temp.y = point(1);
-        temp.z = point(2);
+        temp.x = point.x();
+        temp.y = point.y();
+        temp.z = point.z();
 
-        publishSphere(temp, color, scale, "com");
+        return publishSphere(temp, color, scale, ns);
     }
 
-    bool ZeroDim::publishEigenSpheres(Eigen::VectorXd & eigen_path_x,
-                                                Eigen::VectorXd & eigen_path_y,
-                                                Eigen::VectorXd & eigen_path_z,
-                                                rviz_visual_tools::colors color,
-                                                rviz_visual_tools::scales scale,
-                                                const std::string & ns)
+    bool ZeroDim::publishEigenSpheres(const Eigen::VectorXd & eigen_path_x,
+                                      const Eigen::VectorXd & eigen_path_y,
+                                      const Eigen::VectorXd & eigen_path_z,
+                                      rviz_visual_tools::colors color,
+                                      rviz_visual_tools::scales scale,
+                                      const std::string & ns)
     {
-
-        geometry_msgs::Point temp;
-        geometry_msgs::Point first;
-        std::vector<geometry_msgs::Point> trajectory;
+    std::vector<geometry_msgs::Point> trajectory;
         int points_num = eigen_path_x.rows();
-
-        for (std::size_t i = 0; i < points_num; ++i)
+        trajectory.reserve(points_num);
+        geometry_msgs::Point temp;
+        for (int i = 0; i < points_num; ++i)
         {
             temp.x = eigen_path_x(i);
             temp.y = eigen_path_y(i);
             temp.z = eigen_path_z(i);
 
-            if (i == 0)
-            {
-                first = temp;
-            }
-
             trajectory.push_back(temp);
         }
-        publishSpheres(trajectory, color, scale, "intermediate_points");
+        return publishSpheres(trajectory, color, scale, ns);
     }
-
-
 }
